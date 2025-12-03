313864c7c84a7b76fc4af0bcbbfed161989ca6c7
Khusi Khilika Majhi <ginamalhotra1412@gmail.com>
2025-12-04 04:54:31 +0530

    Descriptive commit messgage

--- src/recommend.py
+++ src/recommend.py
@@ -2,7 +2,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 def recommend_songs(df, scaler, mood, top_n=5):
-    df["mood"] = df["label"].map({
+    # Map genres to moods
+    mood_map = {
         'pop': 'happy',
         'rock': 'energetic',
         'metal': 'aggressive',
@@ -13,20 +14,33 @@ def recommend_songs(df, scaler, mood, top_n=5):
         'classical': 'peaceful',
         'reggae': 'chill',
         'jazz': 'calm'
-    })
+    }
 
+    # Add mood column
+    df["mood"] = df["label"].map(mood_map)
+
+    # Filter songs with requested mood
     mood_df = df[df["mood"] == mood]
+
     if len(mood_df) == 0:
+        print("⚠️ No songs found for this mood!")
         return []
 
+    # Pick one sample song
     sample = mood_df.sample(1)
-    sample_features = scaler.transform(sample.drop(['filename','length','label','mood'], axis=1))
 
-    features_scaled = scaler.transform(df.drop(['filename','length','label','mood'], axis=1))
-    sim_scores = cosine_similarity(sample_features, features_scaled)[0]
+    feature_columns = df.columns.drop(["filename","length","label","mood"])
+    
+    sample_features = scaler.transform(sample[feature_columns])
+    all_features = scaler.transform(df[feature_columns])
 
-    top_indices = np.argsort(sim_scores)[-top_n:]
+    from sklearn.metrics.pairwise import cosine_similarity
+    sim_scores = cosine_similarity(sample_features, all_features)[0]
+
+    top_indices = sim_scores.argsort()[-top_n:]
+
     return df.iloc[top_indices][["filename", "label", "mood"]]
+
 
 #To RUN it in terminal
 #python src/recommend.py
